b079a85358ecf611d250019c6cd90b2a765d8c11
Quack-Duck12 <jevismaniyar111@gmail.com>
2023-12-03 15:41:49 +0530

    label Placement
    Fixed The Label  Placement and window Geometry

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from customtkinter import *
 
 root = CTk()
 root.title("Python customtkinter Calculator")
-root.geometry("640x750")
+root.geometry("645x750")
 
 Entry = CTkEntry(root,
                  height=120,width=635,
-                 justify = 'right',font=("Stencil Std",25))
+                 justify = 'right',font=("Stencil Std",65))
 
 frame_button = CTkFrame(root,
                         width=615,height=615,
                         bg_color='#555555')
 
-Entry.place(x=5,y=5)
+Entry.place(x=6,y=5)
 frame_button.place(x=15,y=125)
 
 root.mainloop()
