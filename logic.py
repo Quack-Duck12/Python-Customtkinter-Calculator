23df29a102416bbcbdd82cab796cca38107eefb2
Quack-Duck12 <jevismaniyar111@gmail.com>
2023-12-03 15:56:02 +0530

    Started Button Placing

--- logic.py
+++ logic.py
@@ -1,2 +1,5 @@
 from customtkinter import *
 from math import *
+
+def Button_Press(X,Ent):
+    Ent.insert(END,X)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from customtkinter import *
+from logic import Button_Press
 
 root = CTk()
 root.title("Python customtkinter Calculator")
@@ -12,7 +13,19 @@ frame_button = CTkFrame(root,
                         width=615,height=615,
                         bg_color='#555555')
 
+Button_AC = CTkButton(frame_button,
+                     text="AC",
+                     font=("Stencil Std",65),
+                     command=lambda:Button_Press(0,Entry))
+
+Button_1 = CTkButton(frame_button,
+                     text="1",
+                     font=("Stencil Std",65),
+                     command=lambda:Button_Press(1,Entry))
+
 Entry.place(x=6,y=5)
 frame_button.place(x=15,y=125)
+
+Button_AC.pack()
 
 root.mainloop()
